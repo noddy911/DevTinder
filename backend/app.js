70f9c9d3e47625f927c00970560558da1c3e96fc
noddy911 <123327022+noddy911@users.noreply.github.com>
2026-07-31 03:23:53 +0530

    feat: initialize express server with basic user and test routes

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,28 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+  res.json({
+    firstName: "Aman",
+    lastName: "Gupta",
+    age: 20,
+    gender: "Male",
+  });
+});
+
+app.post("/users", (req, res) => {
+  res.send("Data is saved to data base");
+});
 app.use("/test", (req, res) => {
   console.log("This is test route");
   res.send("Hello world");
 });
-app.use("/h", (req, res) => {
-  console.log("This is h route");
-  res.send("Hello from h route");
-});
+// app.use("/h", (req, res) => {
+//   console.log("This is h route");
+//   res.send("Hello from h route");
+// });
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
